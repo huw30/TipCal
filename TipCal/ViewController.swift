4b08ea4abe5bf2f3bb0037324074c630a7cd871c
Raina Wang <raina@weebly.com>
2017-08-07 20:10:20 -0700

    Initial Commit

---
+++ TipCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCal
+//
+//  Created by Raina Wang on 8/7/17.
+//  Copyright © 2017 Raina Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
